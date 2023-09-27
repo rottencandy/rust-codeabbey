09dec2b5b1c3c3d3cb9747754a2b661b60ed6d4a
Mohammed Saud <md.saud020@gmail.com>
2023-09-27 12:50:37 +0530

    skip parameters count from input

--- src/sum_loop.rs
+++ src/sum_loop.rs
@@ -1,6 +1,7 @@
 pub fn run(input: &str) -> i32 {
     input
         .split_whitespace()
+        .skip(1)
         .map(|s| s.parse::<i32>().expect("Unable to parse input"))
         .sum()
 }
@@ -11,6 +12,6 @@ mod tests {
 
     #[test]
     fn test_sum() {
-        assert_eq!(run("2 2 2"), 6);
+        assert_eq!(run("3\n2 2 2"), 6);
     }
 }
