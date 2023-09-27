e190e0aa1f61d896bc11123114afa93d0d91cc0e
Mohammed Saud <md.saud020@gmail.com>
2023-09-27 12:39:21 +0530

    add multiline input support

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,20 @@ use std::{collections::HashMap, io};
 
 fn main() {
     let table = HashMap::from([(1, sum::run as fn(&str) -> i32), (2, sum_loop::run)]);
-    let mut input = String::new();
 
     println!("Problem: ");
+    let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read input");
-    input.clear();
     let problem = input.trim().parse::<i32>().unwrap_or(table.len() as i32);
     let solver = table
         .get(&problem)
         .expect("Solution might not be implemented");
 
-    println!("Input: ");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
-    let out = solver(&input.trim());
+    println!("Input (Ctrl-D to end): ");
+    let input = io::stdin().lines().map(|l| l.unwrap()).collect::<String>();
+    let out = solver(input.trim());
 
     println!("Answer: {}", out);
 }
